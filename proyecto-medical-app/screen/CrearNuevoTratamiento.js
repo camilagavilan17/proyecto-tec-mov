e8e8600d37a10d9b8ee8c452b817e34e9a9f01da
Pablo <valenzuelapablo1998@gmail.com>
2022-05-27 23:07:23 -0400

    update

--- proyecto-medical-app/screen/CrearNuevoTratamiento.js
+++ proyecto-medical-app/screen/CrearNuevoTratamiento.js
@@ -1,23 +1,29 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
 import { BlurView } from 'expo-blur';
 import { useNavigation } from '@react-navigation/native';
 
 export default function CrearNuevoTratamiento() {
     const navigation = useNavigation();
-    const [nombre, setNombre] = React.useState('')
-
+    const [tratamiento, setTratamiento] = useState({
+        nombre: "",
+        fechaInicio: "",
+        fechaTermino:"",
+    });
+    const handleChangeText = (variable, value) => {
+        setTratamiento({...tratamiento, [variable]: value});
+    }
     const pressCrear = () => {
     console.log("Nombre:");
-    console.log(nombre);
+    console.log(tratamiento);
     console.log("Press crear");
-    navigation.navigate('Home');
+    //navigation.navigate('Home');
     
     }
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Nombre del tratamiento</Text>
-            <TextInput style={{borderWidth:1, padding:7, width: 300}} onChangeText={(val) => setNombre(val)}/>
+            <TextInput style={{borderWidth:1, padding:7, width: 300}} onChangeText={(val) => handleChangeText("nombre" ,val)}/>
             <TouchableOpacity onPress={pressCrear}
              style={{width: 250,
                 height: 40,
